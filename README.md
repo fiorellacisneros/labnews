8f50f20edb929882d715387baffce88d434899d8
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-07 10:46:24 -0500

    add resources

---
+++ README.md
@@ -0,0 +1,3 @@
+# LABORATORIA NEWS
+
+
---
+++ src/assets/js/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const render = (root) => {
+    root.empty();
+    const wrapper = $('<div class="wrapper"></div>');
+    wrapper.append(state.url(_ => render(root))); 
+
+    root.append(wrapper);
+};
+
+const state = {
+    url: Header
+};
+
+$( _ => {
+    const root = $('.root');
+    render(root);
+});
