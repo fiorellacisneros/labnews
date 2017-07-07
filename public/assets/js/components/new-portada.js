84496d43450f85c24f21e66f4cf5f8928153a758
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-07 17:49:23 -0500

    add styles new portada

--- public/assets/js/components/new-portada.js
+++ public/assets/js/components/new-portada.js
@@ -1,8 +1,12 @@
 const newPortada = () => {
     const container = $('<div class="container newportada"></div>');
     const imgportada = $(`<img src="assets/img/news/${state.news[0].img}"/>`);
-    
+    const infonew = $('<h3>'+state.news[0].title +'</h3>');
+    const brief = $('<p>'+ state.news[0].brief +'</p>');
+          
     container.append(imgportada);
+    container.append(infonew);
+    container.append(brief);
     
     return container;
 }
