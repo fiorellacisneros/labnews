72086ddbcdbb7bf53ca0bd04ef8995cda160ebf0
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-07 12:09:45 -0500

    add header

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,6 +3,7 @@
 const render = (root) => {
     root.empty();
     const wrapper = $('<div class="wrapper"></div>');
+    //wrapper.append(Header());
     wrapper.append(state.url(_ => render(root))); 
 
     root.append(wrapper);
