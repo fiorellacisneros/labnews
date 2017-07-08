84b1a1f081f468f7252f42da9b4242cfe74ff5a1
Fiorellacr24 <fiorella.0624@gmail.com>
2017-07-07 21:33:57 -0500

    add styles footer

--- public/assets/js/components/footer.js
+++ public/assets/js/components/footer.js
@@ -2,12 +2,12 @@ const Footer = () => {
     const footer = $('<footer class="container"></footer>');
     const top = $('<p class="pull-right"><a href="#">Subir</a></p>');
     const row = $('<div class="row"></div>');
-    const footerleft = $('<div class="col-md-4 footer-left"></div>');
+    const footerleft = $('<div class="col-md-4 footer__left"></div>');
     const imgfootleft = $('<img src="assets/img/logo-footer.png" alt="logofooter">');
     const textfootleft = $('<p>© Editado por Laboratoria Av. José Pardo #601 Lima 1 Perú Copyright © Laboratoria.la         Todos los derechos reservados.</p>');
-    const footercenter = $('<div class="col-md-4 footer-center"></div>');
-    const textfootcenter = $('<ul><li>DIRECTOR GENERAL:</li><li>Francisco Miró Quesada Cantuarias</li><li>DIRECTOR PERIODÍSTICO:</li><li>Fernando Berckemeyer Olaechea</li><li>SUSCRIPCIONES:</li> <li>suscriptores@comercio.com.pe</li><li>PUBLICIDAD ONLINE:</li><li>fonoavisos@comercio.com.pe</li><li>CLUB EL COMERCIO:</li><li>clubelcomercio@comercio.com.pe</li><li>Compromiso de Autorregulación Comercial</li></ul>');
-    const footer_right = $('<div class="col-md-4 footer-rigth"></div>');
+    const footercenter = $('<div class="col-md-4 footer__center"></div>');
+    const textfootcenter = $('<ul><li>DIRECTOR GENERAL:</li><li class="pd-bt-12">Francisco Miró Quesada Cantuarias</li><li>DIRECTOR PERIODÍSTICO:</li><li class="pd-bt-12">Fernando Berckemeyer Olaechea</li><li>SUSCRIPCIONES:</li> <li class="pd-bt-12">suscriptores@comercio.com.pe</li><li>PUBLICIDAD ONLINE:</li><li class="pd-bt-12">fonoavisos@comercio.com.pe</li><li>CLUB EL COMERCIO:</li><li>clubelcomercio@comercio.com.pe</li><li>Compromiso de Autorregulación Comercial</li></ul>');
+    const footer_right = $('<div class="col-md-4 footer__right"></div>');
     const textfootright = $('<ul><li>TÉRMINOS Y CONDICIONES DE USO</li><li>TRABAJA CON NOSOTROS</li><li>LIBRO DE RECLAMACIONES</li><li>SEDES PRINCIPALES</li><li>POLÍTICAS DE PRIVACIDAD</li></li></ul>');
     
     footer.append(top);
